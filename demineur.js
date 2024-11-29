c1749f127de61663f2681184e2809c5310ee00a5
Bocquel Raphael <bocquel@insa-21049>
2024-11-29 08:59:08 +0100

    Ajout d'images

--- demineur.js
+++ demineur.js
@@ -4,6 +4,30 @@ class Case {
 	this.posy = y;
 	this.isMine = false;
 	this.number = 0;
+	this.hidden = true;
     }
 
+    get imageName() {
+	return this.getImageName();
+    }
+    
 }
+
+let grille = [];
+
+function creerGrille(haut, larg) {
+    grille.length = haut;
+    let i = 0;
+    let j = 0;
+    while (i < haut) {
+	grille[i] = Array(larg);
+	j = 0;
+	while (j<larg) {
+	    grille[i][j] = new Case(j, i);
+	    j = j + 1 ;
+	}
+	i = i + 1;
+    }
+    
+}
+

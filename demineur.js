66296c36ac7e9ab2a3f74699351d84b0fb1cdeca
Bezza Younes <bezza@srv-tp06>
2024-12-05 20:20:52 +0100

    timer ajouté

--- demineur.js
+++ demineur.js
@@ -7,3 +7,33 @@ class Case {
     }
 
 }
+function sleep(ms){
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function chrono(){
+	let k=document.getElementById("temps").value;
+	const myButton = document.getElementById("fin_game");
+	let arret=0;
+
+	while(arret==!1) {
+	
+	await sleep(1000);
+	document.getElementById("temps").value=document.getElementById("temps").value*1+1;
+	myButton.addEventListener("click",() =>{arret=1;})
+}
+}
+async function reset(){
+	await sleep(500);
+	document.getElementById("temps").value=0;
+
+}
+
+function Cookies() {
+	var accept = confirm("Tu veux des cookies ?");
+	if (accept === true) {
+	alert("Bon jeu!");
+	} else {
+	Cookies();
+	}
+	}
+Cookies();

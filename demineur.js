8b895ee3830b5c2a62dc161a8b6bd4d5f9d4c037
bezza@insa-toulouse.fr <bezza@insa-toulouse.fr>
2024-12-25 15:24:46 +0100

    finalisation popup cookie

--- demineur.js
+++ demineur.js
@@ -8,18 +8,6 @@ class Case {
     }
 
 }
-<<<<<<< HEAD
-=======
-//Fonction pour créer un cookie.
-function setCookie(name, value, days) {
-    const expires = new Date(Date.now() + days * 864e5).toUTCString();
-    document.cookie = `${name}=${value}; expires=${expires}; path=/`;
-}
-// Fonction pour lire un cookie
-function getCookie(name) {
-    return document.cookie.split('; ').find(row => row.startsWith(name + '='))?.split('=')[1];
-}
->>>>>>> d322df708ea3bd0301a054980adc49db1c96d181
 
 function sleep(ms){
 	return new Promise(resolve => setTimeout(resolve, ms));
@@ -44,15 +32,7 @@ async function reset(){
 
 }
 
-function Cookies() {
-	var accept = confirm("Tu veux des cookies ?");
-	if (accept === true) {
-	alert("Bon jeu!");
-	} else {
-	Cookies();
-	}
-	}
-//Cookies();
+
 
 
 let grille = [];
@@ -70,21 +50,32 @@ function creerGrille(haut, larg) {
     
 }
 
+// Fonction pour créer un cookie
+function setCookie(name, value, days) {
+    const expires = new Date(Date.now() + days * 864e5).toUTCString();
+    document.cookie = `${name}=${value}; expires=${expires}; path=/`;
+}
 
+// Fonction pour lire un cookie
+function getCookie(name) {
+    return document.cookie.split('; ').find(row => row.startsWith(name + '='))?.split('=')[1];
+}
 
 
 function cookie(){
 	const popup = document.getElementById('popup');
-	const closePopupButton = document.getElementById('accept-button');
-	
+	const close_popup_button = document.getElementById('accept-button');
+	const keep_popup_button = document.getElementById('refuse-button');
 	// Fonction pour afficher la pop-up
-	
 	popup.classList.add('show');
-	
+
+	keep_popup_button.addEventListener('click', () => {
+		keep_popup_button.style.display = 'none';
+	});
 	
 	// Fonction pour fermer la pop-up
-	closePopupButton.addEventListener('click', () => {
-		setCookie('cookiesAccepted', 'true', 1); // Stocke l'acceptation pendant 1 jour
+	close_popup_button.addEventListener('click', () => {
+		// setCookie('cookiesAccepted', 'true', 1); // Stocke l'acceptation pendant 1 jour
 		popup.classList.remove('show');
 	});
 	
